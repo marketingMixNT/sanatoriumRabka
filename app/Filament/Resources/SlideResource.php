bb2c45a666cc685dc47fe9e6af3d888c76269085
marekgacek45 <marekgacek45@gmail.com>
2024-10-08 08:07:07 +0200

    update slide resource

--- app/Filament/Resources/SlideResource.php
+++ app/Filament/Resources/SlideResource.php
@@ -10,6 +10,7 @@ use Filament\Tables\Table;
 use Illuminate\Support\Str;
 use Filament\Resources\Resource;
 use Awcodes\Shout\Components\Shout;
+use Filament\Forms\Components\Fieldset;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\SlideResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -35,56 +36,71 @@ class SlideResource extends Resource
                     ->color('success')
                     ->columnSpanFull(),
 
-                Forms\Components\FileUpload::make('image')
-                    ->label('Zdjęcie')
-                    ->directory('slides')
-                    ->getUploadedFileNameForStorageUsing(
-                        fn(TemporaryUploadedFile $file): string => 'hotel-jan-' . now()->format('Ymd_His') . '.' . $file->getClientOriginalExtension()
-                    )
-                    ->image()
-                    ->maxSize(4096)
-                    ->optimize('webp')
-                    ->imageEditor()
-                    ->imageEditorAspectRatios([
-                        null,
-                        '16:9',
-                        '4:3',
-                        '1:1',
-                    ])
-                    ->required(),
+                // PHOTO
+                Fieldset::make('Zdjęcie')
+                    ->schema([
+                        Forms\Components\FileUpload::make('image')
+                            ->label('')
+                            ->directory('slides')
+                            ->getUploadedFileNameForStorageUsing(
+                                fn(TemporaryUploadedFile $file): string => 'sanatorium-rabka-' . now()->format('H-i-s') . '-' . str_replace([' ', '.'], '', microtime()) . '.' . $file->getClientOriginalExtension()
+                            )
+                            ->image()
+                            ->maxSize(4096)
+                            ->optimize('webp')
+                            ->imageEditor()
+                            ->columnSpanFull()
+                            ->imageEditorAspectRatios([
+                                null,
+                                '16:9',
+                                '4:3',
+                                '1:1',
+                            ])
+                            ->required(),
 
-                Forms\Components\TextInput::make('alt')
-                    ->label('Tekst alternatywny')
-                    ->required()
-                    ->minLength(3)
-                    ->maxLength(255)
-                    ->helperText('Opisz w jednym zdaniu co znajduje się na obrazku aby poprawić SEO'),
+                        Forms\Components\TextInput::make('alt')
+                            ->label('Tekst alternatywny')
+                            ->required()
+                            ->minLength(3)
+                            ->maxLength(255)
+                            ->helperText('Opisz w jednym zdaniu co znajduje się na obrazku aby poprawić SEO'),
+                    ]),
 
-                Forms\Components\TextInput::make('heading')
-                    ->label('Tytuł')
-                    ->columnSpanFull()
-                    ->minLength(3)
-                    ->maxLength(255)
-                    ->required(),
+                // CONTENT
+                Fieldset::make('Treść')
+                    ->schema([
+                        Forms\Components\TextInput::make('heading')
+                            ->label('Tytuł')
+                            ->columnSpanFull()
+                            ->minLength(3)
+                            ->maxLength(255)
+                            ->required(),
 
-                Forms\Components\Textarea::make('text')
-                    ->label('Treść')
-                    ->columnSpanFull()
-                    ->rows(5)
-                    ->required(),
-                    
-                    Forms\Components\TextInput::make('link')
-                    ->label('Link')
-                    ->columnSpanFull()
-                    ->url()
-                    ->required(),
-                    Forms\Components\TextInput::make('button_text')
-                    ->label('Tekst w przycisku')
-                    ->placeholder('np. zobacz')
-                    ->columnSpanFull()
-                    ->minLength(3)
-                    ->maxLength(255)
-                    ->required(),
+                        Forms\Components\Textarea::make('text')
+                            ->label('Tekst')
+                            ->columnSpanFull()
+                            ->rows(3)
+                            ->required(),
+                    ]),
+
+                // LINK & BUTTON
+                Fieldset::make('Treść')
+                    ->schema([
+                        Forms\Components\TextInput::make('link')
+                            ->label('Link')
+                            ->columnSpanFull()
+                            ->url()
+                            ->required(),
+                        Forms\Components\TextInput::make('button_text')
+                            ->label('Tekst w przycisku')
+                            ->placeholder('np. zobacz ofertę')
+                            ->columnSpanFull()
+                            ->minLength(3)
+                            ->maxLength(255)
+                            ->required(),
+                    ]),
+
+
 
             ]);
     }
@@ -127,6 +143,7 @@ class SlideResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- resources/views/pages/home/partials/slider.blade.php
+++ resources/views/pages/home/partials/slider.blade.php
@@ -6,14 +6,15 @@
         {
         <div class="swiper-slide relative w-full h-full ">
             <div class="max-w-screen-xl mx-auto flex justify-start items-center h-full pb-24">
-                <div class="h-full  max-w-[600px] flex flex-col justify-center items-start gap-8  z-10 text-fontLight px-6 lg:px-0">
+                <div
+                    class="h-full  max-w-[600px] flex flex-col justify-center items-start gap-8  z-10 text-fontLight px-6 lg:px-0">
                     <h1 class="text-5xl font-medium">{{$slide->heading}}</h1>
                     <p class="text-base">{{$slide->text}}</p>
-                    <x-link-button href="{{$slide->link}}">Sprawdź</x-link-button>
+                    <x-link-button href="{{$slide->link}}">{{$slide->button_text}}</x-link-button>
                 </div>
             </div>
 
-            <img src="{{asset('storage/' .  $slide->image)}}" alt="{{$slide->heading}}"
+            <img src="{{asset('storage/' .  $slide->image)}}" alt="{{$slide->alt}}"
                 class="absolute inset-0 w-full h-full object-cover" />
 
             <div class="absolute inset-0 bg-black opacity-50"></div>
@@ -27,10 +28,10 @@
             class="be-panel hidden md:block absolute bottom-44 left-0 right-0 md:mx-6  lg:mx-32 2xl:mx-0 2xl:left-1/2 2xl:transform 2xl:-translate-x-1/2 bg-white">
         </div>
         <x-link-button class="md:hidden absolute mt-12 bottom-32 left-1/2 transform -translate-x-1/2"
-            href="https://booking.profitroom.com/pl/aparthoteljan/home?currency=PLN" target="_blank"
-            aria-label="Rezerwuj">Zarezerwuj</x-link-button>
+            href="https://booking.profitroom.com/pl/sanatoriumcegielskirabkazdroj/home?currency=PLN&codes=test"
+            target="_blank" rel="noreferrer nofollow" aria-label="Rezerwuj">Zarezerwuj</x-link-button>
 
-           
+
 
         {{-- ANCHOR --}}
         <a href="#o-nas" class="absolute bottom-6 2xl:bottom-12 left-1/2 transform -translate-x-1/2 z-50"
